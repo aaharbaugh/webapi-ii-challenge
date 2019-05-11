67818a66449d2ca47cd2a448c927d74c62188b56
aaharbaugh <aaronmharbaugh@gmail.com>
2019-05-11 12:39:14 -0500

    added posts display on app.js page

--- client/src/App.js
+++ client/src/App.js
@@ -26,20 +26,9 @@ componentDidMount(){
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>  
-        </header>
+        {this.state.posts.map(post => 
+          <div>{post.id} <strong>{post.title}</strong> {post.contents}</div>
+        )}
       </div>
     );
 
